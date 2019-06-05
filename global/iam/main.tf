3fc84d7749640f9462763a07a1f44d989dec1f39
Brad <brad.anderson58@gmail.com>
2019-06-05 07:00:39 -0400

    ch5 If-Else Statements

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -20,6 +20,22 @@ data "aws_iam_policy_document" "ec2_read_only" {
   }
 }
 
+data "aws_iam_policy_document" "cloudwatch_read_only" {
+  statement {
+    effect    = "Allow"
+    actions   = ["cloudwatch:Describe*", "cloudwatch:Get*", "cloudwatch:List*"]
+    resources = ["*"]
+  }
+}
+
+data "aws_iam_policy_document" "cloudwatch_full_access" {
+  statement {
+    effect    = "Allow"
+    actions   = ["cloudwatch:*"]
+    resources = ["*"]
+  }
+}
+
 resource "aws_iam_user" "example" {
   count = "${length(var.user_names)}"
   name  = "${element(var.user_names, count.index)}"
@@ -30,10 +46,34 @@ resource "aws_iam_policy" "ec2_read_only" {
   policy = "${data.aws_iam_policy_document.ec2_read_only.json}"
 }
 
+resource "aws_iam_policy" "cloudwatch_read_only" {
+  name   = "cloudwatch-read-only"
+  policy = "${data.aws_iam_policy_document.cloudwatch_read_only.json}"
+}
+
+resource "aws_iam_policy" "cloudwatch_full_access" {
+  name   = "cloudwatch-full-access"
+  policy = "${data.aws_iam_policy_document.cloudwatch_full_access.json}"
+}
+
 resource "aws_iam_user_policy_attachment" "ec2_access" {
   count      = "${length(var.user_names)}"
   user       = "${element(aws_iam_user.example.*.name, count.index)}"
   policy_arn = "${aws_iam_policy.ec2_read_only.arn}"
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_full"{
+  count = "${var.give_neo_cloudwatch_full_access}"
+
+  user       = "${aws_iam_user.example.0.name}"
+  policy_arn = "${aws_iam_policy.cloudwatch_full_access.arn}"
+}
+
+resource "aws_iam_user_policy_attachment" "neo_cloudwatch_readonly" {
+  count = "${1 - var.give_neo_cloudwatch_full_access}"
+
+  user       = "${aws_iam_user.example.0.name}"
+  policy_arn = "${aws_iam_policy.cloudwatch_read_only.arn}"
 }
 
 output "neo_arn" {
